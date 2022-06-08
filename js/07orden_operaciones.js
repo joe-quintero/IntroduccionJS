ac4fdd5e99affa23d4fde11413f12c062d9dea3d
Joe Quintero <joedarwin1998@gmail.com>
2022-06-08 13:32:29 -0400

    Orden de las operaciones

---
+++ js/07orden_operaciones.js
@@ -0,0 +1,22 @@
+// Orden de las Operaciones
+
+ let resultado;
+
+resultado = 1 + 9 - 5 * 10
+
+resultado = (1 + 9 - 5)* 10
+
+ console.log(resultado);
+
+
+ // Incrementos
+
+ let puntaje = 10;
+puntaje++; // 10 + 1
+++puntaje; // 1 + 10
+
+puntaje--; // 10 + 1
+--puntaje; // 1 + 10
+
+
+ console.log(puntaje);
