909d84ddfc648b99d03faab2b9e20acdd5606fff
Joe Quintero <joedarwin1998@gmail.com>
2022-06-08 14:07:34 -0400

    Booleans

---
+++ js/09booleans.js
@@ -0,0 +1,15 @@
+// Booleans
+
+const boolean1 = true;
+const boolean2 = false;
+
+
+console.log(typeof boolean1); // typeof indico el tipo de dato
+
+
+console.log(boolean1);
+console.log(boolean2);
+
+const boolean3 = new Boolean(true); // Constructor
+console.log(boolean3);
+
