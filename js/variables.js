bf6831becd182ce713f7051ce32503547f23cca8
Joe Quintero <joedarwin1998@gmail.com>
2022-06-07 22:22:16 -0400

    Variables (var)

---
+++ js/variables.js
@@ -0,0 +1,24 @@
+//comentarios 1 linea
+/*Comentarios varias lineas*/
+
+var producto = 'Aceite Mineral'; //Iniciar la variable con valor
+var producto2; // Iniciar variable sin valor
+
+producto = true; // Se le puede cambiar el tipo de dato
+
+Producto = 10 // El ";" no es indispensable
+
+
+var producto3 = 'Filtro',
+	producto3 = 'Bateria',
+	producto4 = 'Bujia';
+
+// Las variables no pueden comenzar con numeros o caracteres especiales (algunas excepciones)
+
+// Estilos para las variables
+var nombre_producto; // Underscore
+var nombreProducto; // Camelcase (Mas Comun en JavaScript)
+var NombreProducto; // Pascal Case (Mas usado para las Clases)
+var nombreproducto; // No es muy utilizado
+
+console.log(producto);
