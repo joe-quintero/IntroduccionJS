ef5a3cdbc29aabe63ae57598066dba9c9d0e3c23
Joe Quintero <joedarwin1998@gmail.com>
2022-06-08 13:53:31 -0400

    Concatenación y Template Strings

---
+++ js/08conca_template.js
@@ -0,0 +1,13 @@
+const nombre = 'Joe';
+const email = 'correo@correo.com';
+
+
+// Concatenacion
+console.log('Nombre Cliente: '+nombre+' Correo: '+email);
+console.log('Nombre Cliente: ',nombre,' Correo: ',email);
+
+// Template String - String Literal
+
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
+
+
