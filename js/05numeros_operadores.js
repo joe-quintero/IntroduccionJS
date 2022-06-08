45fabfc2074970d433bc56970560e544b0f6adab
Joe Quintero <joedarwin1998@gmail.com>
2022-06-08 12:26:09 -0400

    Numeros y Operadores

---
+++ js/05numeros_operadores.js
@@ -0,0 +1,13 @@
+//Numeros y Operadores
+
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 300.20;
+const numero4 = .400;
+const numero5 = -500; //los numeros no se dividen en enteros ni flotantes, unicamente numero
+
+console.log(numero1 + numero2); // Suma
+console.log(numero2 - numero2); // Resta
+console.log(numero3 * numero2); // Multiplicacion
+console.log(numero4 / numero2); // Divicion
+console.log(10 % 2); // Modulo
