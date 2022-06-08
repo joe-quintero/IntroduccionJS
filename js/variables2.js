c6ee34cd448ff82732bbbfefef4af23dfacbbe93
Joe Quintero <joedarwin1998@gmail.com>
2022-06-07 22:37:29 -0400

    Variables let y const

---
+++ js/variables2.js
@@ -0,0 +1,24 @@
+//variables con  let
+
+//comentarios 1 linea
+/*Comentarios varias lineas*/
+
+let producto = 'Aceite Mineral'; //Iniciar la variable con valor
+let producto2; // Iniciar variable sin valor
+
+producto = true; // Se le puede cambiar el tipo de dato
+
+Producto = 10 // El ";" no es indispensable
+
+
+let producto3 = 'Filtro',
+	producto3 = 'Bateria',
+	producto4 = 'Bujia';
+
+// Las variables no pueden comenzar con numeros o caracteres especiales (algunas excepciones)
+
+// Estilos para las variables
+let nombre_producto; // Underscore
+let nombreProducto; // Camelcase (Mas Comun en JavaScript)
+let NombreProducto; // Pascal Case (Mas usado para las Clases)
+let nombreproducto; // No es muy utilizado
---
+++ js/variables3.js
@@ -0,0 +1,27 @@
+//variables con const
+
+//comentarios 1 linea
+/*Comentarios varias lineas*/
+
+/*
+- No se pueden inicializar sin valor.
+- No se les puede cambiar el valor luego de declararlas
+*/
+
+
+
+const producto = 'Aceite Mineral' //Iniciar la variable con valor
+
+
+
+const producto2 = 'Filtro',
+	producto3 = 'Bateria',
+	producto4 = 'Bujia';
+
+// Las variables no pueden comenzar con numeros o caracteres especiales (algunas excepciones)
+
+// Estilos para las variables
+const nombre_producto = 'Aceite1'; // Underscore
+const nombreProducto = 'Aceite2' // Camelcase (Mas Comun en JavaScript)
+const NombreProducto = 'Aceite3'; // Pascal Case (Mas usado para las Clases)
+const nombreproducto = 'Aceite4'; // No es muy utilizado
