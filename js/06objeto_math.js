d5e7a258e1b1f5920f9447b5ed99e38c4d927585
Joe Quintero <joedarwin1998@gmail.com>
2022-06-08 13:04:15 -0400

    Objeto Math

---
+++ js/06objeto_math.js
@@ -0,0 +1,18 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI; // Valor de PI
+resultado = Math.round(2.6); // Reondeda
+resultado = Math.ceil(2.1); // Siempre redondea hacia arriba
+resultado = Math.floor(2.9); // Siempre redondea hacia abajo
+resultado = Math.sqrt(25); // Raiz cuadrada
+resultado = Math.abs(-200); // Convierte a positivo
+resultado = Math.min(2,5,8,10,8,1); // Retorna en numero mas bajo
+resultado = Math.max(2,5,8,10,8,1); // Retorna el numero mas alto
+resultado = Math.random(); // Numero aleatorio siempre menores a 0.9...
+resultado = Math.floor(Math.random () *30); // Se mezcla para logar otros resultados
+
+
+
+console.log(resultado);
