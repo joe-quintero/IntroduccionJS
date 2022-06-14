91ee788f4e3d3a6cfb0b043291b09e14f42fb160
Joe Quintero <joedarwin1998@gmail.com>
2022-06-14 16:42:57 -0400

    Diferencia de un Método y una función

--- js/20metodo_funcion.js
+++ js/20metodo_funcion.js
@@ -1,4 +1,10 @@
  // Diferencia entre Metodo y Funcion
 // **Terminan siendo practicamente lo mismo, la forma de uso depende mas que nada en el contexto que son utilizadas 
 
+const numero1 = 20;
+const numero2 = '20';
+
+console.log(parseInt(numero2)); // nombre() --> Funcion
+console.log(numero1.toString()); // variable.nombre() --> Metodo
+
 
