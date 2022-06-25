0ea40404bf295a8c01483a1f077ea5a73d62eed5
Joe Quintero <joedarwin1998@gmail.com>
2022-06-25 12:30:30 -0400

    Try Catch

---
+++ js/35tryCatch.js
@@ -0,0 +1,14 @@
+// Try Catch ** Funciona para mostrar el error que se esta dando pero dejando que se ejecute el resto del codigo
+
+const num1 = 1;
+const num3 = 3;
+
+console.log(num1);
+
+try{
+	console.log(num2);
+}catch(error){
+	console.log(error);
+}
+
+console.log(num3);
